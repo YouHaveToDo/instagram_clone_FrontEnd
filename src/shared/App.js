fe4887fabba286f1e49cf9bcb71b09f830c4feea
youHaveToDo <87919383+youHaveToDo@users.noreply.github.com>
2021-12-13 23:06:07 +0900

    merge start

--- src/shared/App.js
+++ src/shared/App.js
@@ -10,28 +10,11 @@ function App() {
   return (
     <React.Fragment>
       <GlobalStyles />
-      <MobileBox>
-        <ConnectedRouter history={history}>
-          {/* 여기서부터 컴포넌트 넣으시면 됩니다.  */}
-        </ConnectedRouter>
-      </MobileBox>
+      <ConnectedRouter history={history}>
+        {/* 여기서부터 컴포넌트 넣으시면 됩니다.  */}
+      </ConnectedRouter>
     </React.Fragment>
   );
 }
-
-const MobileBox = styled.div`
-  max-width: 414px;
-  max-height: 896px;
-  width: 98%;
-  height: 98%;
-  border-radius: 30px;
-  border: 2px solid #efefef;
-  background-color: #ffffff;
-  position: absolute;
-  top: 50%;
-  left: 50%;
-  transform: translate(-50%, -50%);
-  overflow: hidden;
-`;
 
 export default App;
